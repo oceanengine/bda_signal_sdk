c0adb24765623d8de78df1571009bc1451028c30
AchillesL2398 <chenyuzhong.tian@bytedance.com>
2024-04-16 15:27:36 +0800

    feat：使用低版本pb

--- BDASignalSDK/Classes/Utilities/BDASignalUtility.m
+++ BDASignalSDK/Classes/Utilities/BDASignalUtility.m
@@ -48,7 +48,7 @@
     pbHeader.deviceMntId = [self mntID];
     pbHeader.deviceInitTime = [self deviceInitTime];
     pbHeader.userAgent = [self webviewUA];
-    pbHeader.sdkVersion = @"1.0.1";
+    pbHeader.sdkVersion = @"1.0.1-pb-low";
     pbHeader.clickId = [BDASignalManager getClickId];
     
     NSDictionary *appInfo = [[NSBundle mainBundle] infoDictionary];
